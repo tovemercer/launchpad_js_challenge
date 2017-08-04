c8a9c020b93fb6eb7ae95c444b3a8a5e586ca2b7
Tove Mercer <tovemercer@gmail.com>
2017-08-03 22:50:54 -0500

    scoreing feature working and displaying overall score on page.

--- application.js
+++ application.js
@@ -5,26 +5,30 @@ var frameworks = {
   vuejs: "vue"
 }
 
-// console.log(frameworks);
-// console.log(Object.keys(frameworks));
-// console.log(Object.values(frameworks));
-
-// for (key in frameworks[0]) {
-//   console.log(frameworks[0][key])
-// }
-
-
+var calculateScore = function(stars, forks, issues) {
+  var weightedAverage = (stars + forks - (issues * .5)) / 3;
+  var score = weightedAverage / 100;
+  return score.toFixed(0);
+};
 
 $(document).ready(function() {
-
   for (var i in frameworks) {
+    // var $this = this;
+
     $.ajax({
-      url:"https://api.github.com/repos/" + i + "/" + frameworks[i],
+      url: "https://api.github.com/repos/" + i + "/" + frameworks[i],
       data: {
         client_id:'20e837d019d5fe789fb4',
         client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-      }
+      },
+      // success: function(data) {
+      //   if (data.success) {
+      //     console.log($this);
+      //     $('#framework_results').load('#framework_results');
+      //   }
+      // }
     }).done(function(response){
+      var overallScore = calculateScore(response.stargazers_count, response.forks, response.open_issues)
       $('#framework_results').append(`
         <div class="panel panel-default">
           <div class="panel-heading text-center">
@@ -32,7 +36,7 @@ $(document).ready(function() {
           </div>
           <div class="panel-body">
             <ul class="list-unstyled">
-              <li><h4>Overall Score: 98</h4></li>
+              <li><h4>Overall Score: ${overallScore}</h4></li>
               <li>Stars: ${response.stargazers_count}</li>
               <li>Forks: ${response.forks}</li>
               <li>Open Issues: ${response.open_issues}</li>
@@ -42,111 +46,4 @@ $(document).ready(function() {
       `);
     });
   }
-
-
-  // react
-  // $.ajax({
-  //   url:'https://api.github.com/repos/facebook/react',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   $('#framework_results').append(`
-  //     <div class="panel panel-default">
-  //       <div class="panel-heading text-center">
-  //         <h2>${response.name}</h2>
-  //       </div>
-  //       <div class="panel-body">
-  //         <ul class="list-unstyled">
-  //           <li><h4>Overall Score: 98</h4></li>
-  //           <li>Stars: ${response.stargazers_count}</li>
-  //           <li>Forks: ${response.forks}</li>
-  //           <li>Open Issues: ${response.open_issues}</li>
-  //         </ul>
-  //       </div>
-  //     </div>
-  //   `);
-  // });
-
-  // angular
-  // $.ajax({
-  //   url:'https://api.github.com/repos/angular/angular.js',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   $('#framework_results').append(`
-  //     <div class="panel panel-default">
-  //       <div class="panel-heading text-center">
-  //         <h2>${response.name}</h2>
-  //       </div>
-  //       <div class="panel-body">
-  //         <ul class="list-unstyled">
-  //           <li><h4>Overall Score: 98</h4></li>
-  //           <li>Stars: ${response.stargazers_count}</li>
-  //           <li>Forks: ${response.forks}</li>
-  //           <li>Open Issues: ${response.open_issues}</li>
-  //         </ul>
-  //       </div>
-  //       </div>
-  //     </div>
-  //   `);
-  // });
-
-  // ember
-  // $.ajax({
-  //   url:'https://api.github.com/repos/emberjs/ember.js',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   $('#framework_results').append(`
-  //     <div class="panel panel-default">
-  //       <div class="panel-heading text-center">
-  //         <h2>${response.name}</h2>
-  //       </div>
-  //       <div class="panel-body">
-  //         <ul class="list-unstyled">
-  //           <li><h4>Overall Score: 98</h4></li>
-  //           <li>Stars: ${response.stargazers_count}</li>
-  //           <li>Forks: ${response.forks}</li>
-  //           <li>Open Issues: ${response.open_issues}</li>
-  //         </ul>
-  //       </div>
-  //       </div>
-  //     </div>
-  //   `);
-  // });
-
-  // vue
-  // $.ajax({
-  //   url:'https://api.github.com/repos/vuejs/vue',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   $('#framework_results').append(`
-  //     <div class="panel panel-default">
-  //       <div class="panel-heading text-center">
-  //         <h2>${response.name}</h2>
-  //       </div>
-  //       <div class="panel-body">
-  //         <ul class="list-unstyled">
-  //           <li><h4>Overall Score: 98</h4></li>
-  //           <li>Stars: ${response.stargazers_count}</li>
-  //           <li>Forks: ${response.forks}</li>
-  //           <li>Open Issues: ${response.open_issues}</li>
-  //         </ul>
-  //       </div>
-  //       </div>
-  //     </div>
-  //   `);
-  // });
-
-
-
 });
