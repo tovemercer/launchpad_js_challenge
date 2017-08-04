da20813881ef2ba2ded2d1b732c0058f17620440
Tove Mercer <tovemercer@gmail.com>
2017-08-03 19:22:24 -0500

    sll frameworks added and displaying on page. need logic to determine overal score of framework

--- application.js
+++ application.js
@@ -8,65 +8,100 @@ $(document).ready(function() {
       client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
     }
   }).done(function(response){
-    $('#framework_results').html(`
+    $('#framework_results').append(`
       <div class="panel panel-default">
-        <div class="panel-heading">
+        <div class="panel-heading text-center">
           <h2>${response.name}</h2>
         </div>
         <div class="panel-body">
-          <h4>Overall Score: 98</h4><br>
-          Watchers: ${response.watchers}<br>
-          Forks: ${response.forks}<br>
-          Open Issues: ${response.open_issues}
+          <ul class="list-unstyled">
+            <li><h4>Overall Score: 98</h4></li>
+            <li>Stars: ${response.stargazers_count}</li>
+            <li>Forks: ${response.forks}</li>
+            <li>Open Issues: ${response.open_issues}</li>
+          </ul>
         </div>
       </div>
     `);
   });
 
   // angular
-  // $.ajax({
-  //   url:'https://api.github.com/repos/angular/angular.js',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   $('#framework_results').html(`
-  //     <div class="panel panel-default">
-  //       <div class="panel-heading">
-  //         <h2>${response.name}</h2>
-  //       </div>
-  //       <div class="panel-body">
-  //         <h4>Overall Score: 98</h4><br>
-  //         Watchers: ${response.watchers}<br>
-  //         Forks: ${response.forks}<br>
-  //         Open Issues: ${response.open_issues}
-  //       </div>
-  //     </div>
-  //   `);
-  // });
+  $.ajax({
+    url:'https://api.github.com/repos/angular/angular.js',
+    data: {
+      client_id:'20e837d019d5fe789fb4',
+      client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
+    }
+  }).done(function(response){
+    $('#framework_results').append(`
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">
+          <h2>${response.name}</h2>
+        </div>
+        <div class="panel-body">
+          <ul class="list-unstyled">
+            <li><h4>Overall Score: 98</h4></li>
+            <li>Stars: ${response.stargazers_count}</li>
+            <li>Forks: ${response.forks}</li>
+            <li>Open Issues: ${response.open_issues}</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    `);
+  });
 
   // ember
-  // $.ajax({
-  //   url:'https://api.github.com/repos/emberjs/ember.js',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   console.log(response);
-  // });
+  $.ajax({
+    url:'https://api.github.com/repos/emberjs/ember.js',
+    data: {
+      client_id:'20e837d019d5fe789fb4',
+      client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
+    }
+  }).done(function(response){
+    $('#framework_results').append(`
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">
+          <h2>${response.name}</h2>
+        </div>
+        <div class="panel-body">
+          <ul class="list-unstyled">
+            <li><h4>Overall Score: 98</h4></li>
+            <li>Stars: ${response.stargazers_count}</li>
+            <li>Forks: ${response.forks}</li>
+            <li>Open Issues: ${response.open_issues}</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    `);
+  });
 
   // vue
-  // $.ajax({
-  //   url:'https://api.github.com/repos/vuejs/vue',
-  //   data: {
-  //     client_id:'20e837d019d5fe789fb4',
-  //     client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
-  //   }
-  // }).done(function(response){
-  //   console.log(response);
-  // });
+  $.ajax({
+    url:'https://api.github.com/repos/vuejs/vue',
+    data: {
+      client_id:'20e837d019d5fe789fb4',
+      client_secret:'fff3b6fb15af529e9d3675383482607b1bb1771f'
+    }
+  }).done(function(response){
+    $('#framework_results').append(`
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">
+          <h2>${response.name}</h2>
+        </div>
+        <div class="panel-body">
+          <ul class="list-unstyled">
+            <li><h4>Overall Score: 98</h4></li>
+            <li>Stars: ${response.stargazers_count}</li>
+            <li>Forks: ${response.forks}</li>
+            <li>Open Issues: ${response.open_issues}</li>
+          </ul>
+        </div>
+        </div>
+      </div>
+    `);
+  });
 
 
 
